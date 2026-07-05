b816afd3ede0271b8f38d9c8fc5017ec4f9ca666
John Collins <john@bacstitchdna.com>
2026-07-05 09:51:37 -0600

    Add normalize package

---
+++ src/chatgpt_archive_compiler/normalize/__init__.py
@@ -0,0 +1,5 @@
+"""Normalization utilities for source export records."""
+
+from chatgpt_archive_compiler.normalize.conversations import normalize_conversations_payload
+
+__all__ = ["normalize_conversations_payload"]
